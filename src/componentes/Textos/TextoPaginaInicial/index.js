c864a2e2839c2630faf7e4e239b8a4ee9e93e173
Gabriel Rodrigues Ferreira <devoldine2020@gmail.com>
2023-08-16 13:50:45 -0300

    Adicionando ScrollReveal

--- src/componentes/Textos/TextoPaginaInicial/index.js
+++ src/componentes/Textos/TextoPaginaInicial/index.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import style from './minhaFoto.module.css'
+import { motion } from 'framer-motion'
 import minhaFoto from '../../../assets/Minha foto/minha_foto-1.png'
+import ScrollReveal from 'scrollreveal'
 
 export default function MinhaFoto() {
+
+    const elementoRef = useRef(null);
+
+    useEffect(() => {
+        const revelar = ScrollReveal()
+        revelar.reveal(elementoRef.current,{})
+    }, [])
+
+    const list = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                delayChildren: 1,
+                staggerDirection: -1,
+                duration: 2
+            }
+        }
+    }
+
+    const item = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1 }
+    }
+
     return (
         <div className={style.foto}>
-            <img className={style.minhaFoto} src={minhaFoto} />
-            <h1 className={style.textos}>Meu portifólio</h1>
-            <h2 className={style.textos__h2}>
-                Meu nome é Gabriel Rodrigues e é um prazer ter você aqui.
-                Neste espaço, compartilho minhas realizações, habilidades e experiências,
-                apresentando um pouco do meu trabalho e trajetória profissional.</h2>
+            <motion.div variants={list} initial="hidden" animate="show">
+                <img className={style.minhaFoto} src={minhaFoto} />
+            </motion.div>
+            <motion.div animate={{x: 10}}>
+                <h1 className={style.textos}>Meu portifólio</h1>
+                <h2 className={style.textos__h2}>
+                    Meu nome é Gabriel Rodrigues e é um prazer ter você aqui.
+                    Neste espaço, compartilho minhas realizações, habilidades e experiências,
+                    apresentando um pouco do meu trabalho e trajetória profissional.
+                </h2>
+            </motion.div>
         </div>
     )
 }
